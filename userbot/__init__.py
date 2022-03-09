9106b51d9c169e416f9fd74045c5b946ff9a42a7
rezaxd00 <101118418+rezaxd00@users.noreply.github.com>
2022-03-10 00:23:33 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -135,7 +135,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/rezaxd00/ja-ubot")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "ja-ubot")
+    "UPSTREAM_REPO_BRANCH", "IndomieUserbot")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
